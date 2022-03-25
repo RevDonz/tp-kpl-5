2912a3bddb6cb5975b811daaf12ca043e790c178
RevDonz <aprilio842@gmail.com>
2022-03-25 15:59:39 +0700

    Implementasi Design by Contract

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@ internal class MainClass
     public static void Main(string[] args)
     {
         SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design By Contract - Doni");
+        video1.IncreasePlayCount(10);
+        
         video1.PrintVideoDetails();
+
+        SayaTubeVideo video2 = new SayaTubeVideo("Tutorial Handling Exception - Doni");
+        for (int i = 0; i < 215; i++)
+        {
+            video2.IncreasePlayCount(10000000);
+        }
+
+        video2.PrintVideoDetails();
     }
 }
--- SayaTubeVideo.cs
+++ SayaTubeVideo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,20 +17,50 @@ namespace tpmodul5_1302204051
         {
             Random rnd = new Random();
             this.id = rnd.Next(0, 100000);
-            this.title = title;
             this.playCount = 0;
+
+            Debug.Assert(title != null, "title tidak boleh kosong");
+            Debug.Assert(title.Length < 100, "title kepanjangan");
+
+            string test;
+            
+            try
+            {
+                test = checked(title);
+                this.title = title;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
         }
 
         public void IncreasePlayCount(int data)
         {
-            this.playCount += data;
+            Debug.Assert(this.playCount <= int.MaxValue);
+            Debug.Assert(data <= 10000000, "Input tidak boleh lebih dari 10 juta");
+
+            
+            int test;
+            try
+            {
+                test = checked(this.playCount + data);
+                this.playCount += data;
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : " + ex.Message);
+                this.playCount = this.playCount;
+            }
+            
         }
 
         public void PrintVideoDetails()
         {
-            Console.WriteLine(this.id);
-            Console.WriteLine(this.title);
-            Console.WriteLine(this.playCount);
+            Console.WriteLine("Id : " + this.id);
+            Console.WriteLine("Title : " + this.title);
+            Console.WriteLine("Play Count : " + this.playCount + "\n");
         }
     }
 }
